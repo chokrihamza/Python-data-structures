4d13ebe030c289b47f440e759bcdf0193381f7b4
chokrihamza <chokrihamza1994@gmail.com>
2022-06-10 10:32:57 +0200

    add traverse tree

--- Binary_Search_Trees_Traversals_and_Recursion/Balanced_Binary_Search_Trees.py
+++ Binary_Search_Trees_Traversals_and_Recursion/Balanced_Binary_Search_Trees.py
@@ -75,20 +75,122 @@ they all involve traversing a single path down from the root of the tree.
 
 """
 
-
 class TreeNode:
-    def __init__(self,key):
-        self.key=key
-        self.right=None
-        self.left=None
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+Node0=TreeNode(3)
+Node1=TreeNode(4)
+Node2=TreeNode(5)
+Node0.right=Node1
+Node0.left=Node2
+#print(Node0.key,Node0.left.key,Node0.right.key)
+
+# generate this tree using tuples
+"""
+                             2
+                            /  \
+                           3    5
+                          /    /  \
+                         1    3    7
+                               \   / \
+                                4  6  8
+
+"""
+tree_tuple=((1,3,None),2,((None,3,4),5,(6,7,8)))
+
+def parse_tuple(data):
+    #print(data)
+    if isinstance(data,tuple) and len(data)==3:
+        node=TreeNode(data[1])
+        node.left=parse_tuple(data[0])
+        node.right=parse_tuple(data[2])
+    elif data is None:
+        node=None
+    else:
+        node=TreeNode(data)
+
+    return node
+
+tree2=parse_tuple(tree_tuple)
+#print( isinstance(tree2,TreeNode))
+
+# convert tree to tuple
+def tree_to_tuple(node):
+   if isinstance(node,TreeNode):
+        # base case if tree node has no left and right sub_tree
+        if node.left is None and node.right is None:
+            return node.key
+
+        return (tree_to_tuple(node.left),node.key,tree_to_tuple(node.right))
+
+#print(tree_to_tuple(tree2))
+
+#create a helper function to display all the
+# keys in a tree-like structure for easier visualization.
+def display_keys(node,space='\t',level=0):
+    # base case
+    # if the node is empty
+    if node is None:
+        print(space*level+'0')
+        return
+    #if the node is a leaf:
+    if node.left is None and node.right is None :
+        print(space*level+str(node.key))
+        return
+    # if the node has a children
+
+    display_keys(node.right, space, level + 1)
+    print(space*level+str(node.key))
+    display_keys(node.left, space, level + 1)
+
+
+#display_keys(tree2,'   ')
+
+
+# traversing a binary tree
+# three types of traversing
+# 1- inorder
+# 2- preorder
+# 3- postorder
+
+# define a funtion to traverse the tree inorder
 
 
 
+def traverse_in_order(node):
+     if node is None:
+         return []
+     return(
+         traverse_in_order(node.left)+[node.key]+traverse_in_order(node.right)
+     )
 
 
+#print(traverse_in_order(tree2))
 
 
+# define a funtion to traverse the tree preorder
 
 
+def traverse_pre_order(node):
+    if node is None :
+        return []
+    return(
+        [node.key]+traverse_pre_order(node.left)+traverse_pre_order(node.right)
+    )
 
+#print(traverse_pre_order(tree2))
 
+# define a funtion to traverse the tree postorder
+def traverse_post_order(node):
+    if node is None :
+        return []
+    return(
+        traverse_pre_order(node.left)+traverse_pre_order(node.right)+[node.key])
+
+print(traverse_post_order(tree2))
+
+#height and size of a binary tree
