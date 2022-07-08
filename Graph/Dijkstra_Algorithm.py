0ef946c3c1b5b0dfd4650f950ebe0bb623df6084
chokrihamza <chokrihamza1994@gmail.com>
2022-07-08 13:33:38 +0200

    dijkstra algorithm

--- Graph/Dijkstra_Algorithm.py
+++ Graph/Dijkstra_Algorithm.py
@@ -78,3 +78,57 @@ class Graph:
 graph=Graph(num_nodes,edges,directed=True,weighted=True)
 
 print(graph)
+
+
+# update distance function
+
+def update_distance(graph,current,distance,parent=None):
+    """update distance of the current node neighbors"""
+    neighbors=graph.data[current]
+    weights=graph.weight[current]
+    for i,node in enumerate(neighbors):
+        weight=weights[i]
+        if distance[current]+weight<distance[node]:
+            distance[node]=distance[current]+weight
+            if parent:
+                parent[node]=current
+
+
+# pick_next_node
+def pick_next_node(distance,visited):
+    """ pick the next unvisited node at the smallest distance"""
+    min_distance=float('inf')
+    min_node=None
+    for node in range(len(distance)):
+        if not visited[node] and distance[node]<min_distance:
+            min_node=node
+            min_distance=distance[node]
+    return min_node
+
+
+
+# define the function shortest path
+def shortest_path(graph,source,dest):
+    """ Find the length of the shortest path between source and destination"""
+    visited=[False]*len(graph.data)
+    distance=[float('inf')]*len(graph.data)
+    parent=[None]*len(graph.data)
+    queue=[]
+    idx=0
+    queue.append(source)
+    distance[source]=0
+    visited[source]=True
+    while idx<len(queue) and not visited[dest]:
+        current=queue[idx]
+        update_distance(graph,current,distance,parent)
+        next_node=pick_next_node(distance,visited)
+        if next_node is not None:
+            visited[next_node]=True
+            queue.append(next_node)
+        idx+=1
+    print(list(enumerate(parent)))
+    return distance[dest],distance,parent
+
+# test Dijkstra Algorithme
+
+print(shortest_path(graph,0,5))
